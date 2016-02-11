523273c9defea3c8ba601e1671fa587ffcde4eff
nickdevcs4 <sommixaybout@gmail.com>
2016-02-11 15:39:36 +0700

    hao sarng file mai der sue NIck

---
+++ TestRepositoryLTC/NickViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NickViewController.swift
+//  TestRepositoryLTC
+//
+//  Created by Sommixay Boutchanthalath on 2/11/16.
+//  Copyright © 2016 LTC. All rights reserved.
+//
+
+import UIKit
+
+class NickViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
